0e0f2b119a484ba423ee705330cca3f09c7be472
CPearson109 <123629826+CPearson109@users.noreply.github.com>
2023-11-21 15:35:04 +0000

    Delete 4_pacMan directory

--- 4_pacMan/PlayerMovementComponent.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "cmp_actor_movement.h"
-
-class PlayerMovementComponent : public ActorMovementComponent {
-public:
-    explicit PlayerMovementComponent(Entity* p);
-
-    void update(double dt) override;
-};
